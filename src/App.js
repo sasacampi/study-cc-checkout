799cbc09f94798666571ac82d8d48ed16f9d50fc
harverino <sabrinapscampos@gmail.com>
2023-07-03 22:52:29 -0300

    Fixing components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import ReactDOM from "react-dom"
 import Hero from "./components/Hero"
 import "./style.css"
 import Card from "./components/Card"
+import Cart from "./components/Cart"
 
-export default function App() {
-return (
-    <div>
-        <Hero />  
-        <Card /> 
-    </div>
-)
-}
+const App = () => {
+    return (
+      <div className="app">
+        <div className="left-container">
+          <Hero />
+          <Card />
+        </div>
+        <div className="right-container">
+          <Cart />
+        </div>
+      </div>
+    );
+  };
+  
+  export default App;
